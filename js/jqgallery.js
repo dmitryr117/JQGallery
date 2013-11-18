25516cf646c389132cb8bd9fc583a6b997e61cea
dima <dmitry.r117@gmail.com>
2013-11-18 14:15:47 -0600

    release 0.1

---
+++ js/jqgallery.js
@@ -0,0 +1,17 @@
+/*
+ * Author: Dmitry Rodetsky
+ * Business E-mail: info@colirs.ca
+ * E-mail: dmitry.r117@gmail.com
+ * Date: 18 November 2013
+ * 
+ * Version: 0.1
+ * 
+ * This is a slideshow. It zooms images in and out as it pans them.
+ * This solution is built on top of JQuery library 1.10.2
+ * 
+ */
+
+$(document).ready(function() {
+	// initialize position and zoom variables
+	
+});
