df85fc40a493c56df24dfd1684af92dfb8d438ee
dima <dmitry.r117@gmail.com>
2013-11-19 11:52:13 -0600

    crossfade works

--- js/jqgallery.js
+++ js/jqgallery.js
@@ -12,6 +12,35 @@
  */
 
 $(document).ready(function() {
-	// initialize position and zoom variables
-	
+	//jqGallerySlideRotate(1);
+	jqMoveZoom();
 });
+
+function jqGallerySlideRotate(initial) {
+	var imgNum = $('#jqgallery-slideshow img').length;
+	current = initial % imgNum;
+	
+	$('#jqgallery-slideshow img').eq(current).fadeOut(5000, function() {
+		$('#jqgallery-slideshow img').each(function(i) {
+			$(this).css('zIndex', ((imgNum - i) + current) % imgNum);
+		});
+		$(this).show();
+		setTimeout(function() {jqGallerySlideRotate(++current);}, 4000);
+	});
+}
+
+function jqMoveZoom() {
+	var item = $('#jqgallery-slideshow img:first');
+	if(item.length == 0){
+		alert('Error no items in slideshow');
+	}
+	
+	var width = item.attr('width');
+	if (width <= 950){
+		// increase the size of the image
+		
+	}
+}
+
+
+
